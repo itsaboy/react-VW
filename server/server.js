30bf7fc554e88e792f16320a0dfbba4797e8ad55
Zach <itsaboy3366@gmail.com>
2024-02-19 23:33:23 -0500

    server setup complete, security update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import * as url from "url";
+import dotenv from "dotenv";
+dotenv.config({ path: "server/.env" });
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const port = process.env.PORT;
@@ -10,4 +12,8 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
