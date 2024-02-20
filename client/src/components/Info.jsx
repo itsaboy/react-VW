4925e6f9b6a21a34d18a06456c691e999127e9f3
Zach <itsaboy3366@gmail.com>
2024-02-20 16:21:45 -0500

    slight tweak to position of image inside info component

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -91,7 +91,7 @@ export default function Info() {
             </div>
           </div>
           <img
-            className="place-self-start mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-12 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-8 shadow-lg shadow-red-500 transition duration-300 hover:ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-red-500 bg-gray-900"
+            className="place-self-end mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-12 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-8 shadow-lg shadow-red-500 transition duration-300 hover:ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-red-500 bg-gray-900"
             src="/images/void-barb.webp"
             alt="A warrior in space"
           />
